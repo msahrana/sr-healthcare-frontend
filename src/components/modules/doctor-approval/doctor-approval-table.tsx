f7f1202e7df8d6971dbe720a62977bafbfc8e8b4
Sayed Anower Hossain Rana <msahrana@gmail.com>
2026-09-17 12:38:55 +0600

    doctor-approval update

---
+++ src/components/modules/doctor-approval/doctor-approval-table.tsx
@@ -0,0 +1,32 @@
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table';
+import DoctorReviewSheet from './doctor-review-sheet';
+
+export default function DoctorApprovalTable() {
+    return (
+        <div className="border rounded-lg">
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-24">Name</TableHead>
+                        <TableHead className="text-right">Action</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow>
+                        <TableCell className="font-medium">Dr Siyana</TableCell>
+                        <TableCell className="text-right">
+                            <DoctorReviewSheet />
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </div>
+    );
+}
